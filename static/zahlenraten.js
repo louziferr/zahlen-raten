eb739dcf37fb9450752800c84ebe486e264e8633
luzieahrens <luzie.ahrens@gmail.com>
2025-07-15 11:04:48 +0200

    add frontend

--- static/zahlenraten.js
+++ static/zahlenraten.js
@@ -1,8 +1,15 @@
 // Element body per ID abfragen
 const body = document.getElementById("hintergrund");
 
+let blau = true;
+
 function changeColor() {
     console.log('Farbe ändern!');
+
+    if (blau) { body.style.background = "#E97C49"; } 
+    else { body.style.background = "#4c96ad"; }
+    
+    blau = !blau;
 }
 
 // Style von body ändern, wenn er angeklickt wird

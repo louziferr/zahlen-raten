a74686865adb0e5c7eb1a32aec4bb879c54cf880
luzieahrens <luzie.ahrens@gmail.com>
2025-07-16 11:54:27 +0200

    implement game functionality

--- static/zahlenraten.js
+++ static/zahlenraten.js
@@ -1,16 +1,48 @@
 // Element body per ID abfragen
 const body = document.getElementById("hintergrund");
+const tipp = document.getElementById("tipp");
+const inputZahl = document.getElementById("input-zahl");
+const buttonRaten = document.getElementById("button-raten");
+const versuche = document.getElementById("versuche");
 
-let blau = true;
+// zufällige Zahl zwischen 1 und 100 generieren
+const zahl = Math.floor(Math.random() * 100) + 1;
 
-function changeColor() {
-    console.log('Farbe ändern!');
+// Anzahl der Leben
+let leben = 5;
 
-    if (blau) { body.style.background = "#E97C49"; } 
-    else { body.style.background = "#4c96ad"; }
-    
-    blau = !blau;
+// Funktion zum Raten einer Zahl
+function zahlRaten() {
+    if (!inputZahl.checkValidity()) {
+        // Input ist nicht valide
+        alert('Es muss eine Zahl zwischen 1 und 100 eingegeben werden.');
+    } else {
+        // Input ist valide: Zahl überprüfen
+        const geraten = inputZahl.value;
+        if (geraten == zahl) {
+            // Geratene Zahl war richtig!
+            tipp.innerHTML = "Erraten! Die Zahl war: " + zahl;
+        } else {
+            // Falsch geraten
+            if (leben == 0) {
+                // Verloren
+                tipp.innerHTML = "Du hast verloren! Die Zahl war: " + zahl;
+                buttonRaten.setAttribute("disabled", true);
+                buttonRaten.style.background = 'gray';
+                return;
+            }
+            leben--;
+            versuche.innerHTML = "Du hast noch " + leben + " Versuche!";
+            if (geraten < zahl) {
+                // Geratene Zahl ist zu niedrig
+                tipp.innerHTML = "Deine Zahl ist: zu niedrig!";
+            } else {
+                // Geratene Zahl ist zu hoch
+                tipp.innerHTML = "Deine Zahl ist: zu hoch!";
+            }
+        }
+    };
 }
 
-// Style von body ändern, wenn er angeklickt wird
-body.addEventListener('click', changeColor);
+// Event-Listener zum Knopf hinzufügen
+buttonRaten.addEventListener('click', zahlRaten);
